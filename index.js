2bb575c076bb502ae08d97ca1aa1072bbec5ee6c
Romila Rangel <85246670+romilagui@users.noreply.github.com>
2022-05-17 22:07:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+/*****  Challenge *****/
+function getMilk(bottles) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("pick up " + bottles + " bottles of milk " );
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+getMilk(12);
